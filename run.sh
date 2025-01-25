455896c3a6a771d38e1d2a397d0c6bd421681b78
antidianuj <dianujkotov15@gmail.com>
2025-01-25 23:36:11 +0500

    correcting typos

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ source ~/anaconda3/etc/profile.d/conda.sh  # Adjust path to appropiate environme
 conda activate color_detect
 
 
-python main.py  --video_pth $1  --output_pth output_imgs --percent_frames $2
+python main.py  --video_pth $1  --output_pth output_imgs --percent_frames 1.0 --fps 24
